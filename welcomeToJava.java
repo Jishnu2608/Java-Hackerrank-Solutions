a036e1dff36b9d7f4db0b57c95c4eac0a04ca612
Jishnu2608 <jishnudeepborah@gmail.com>
2022-03-06 11:50:42 +0530

    Welcome to Java

---
+++ welcomeToJava.java
@@ -0,0 +1,6 @@
+public class welcomeToJava {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.\nHello, Java.");
+    }
+}
