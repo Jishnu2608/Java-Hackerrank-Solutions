77b638ca10a1cc5c7b51d721134c09db890a06a8
Jishnu2608 <jishnudeepborah@gmail.com>
2022-03-06 21:29:58 +0530

    Java String Tokens

---
+++ stringTokens.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class stringTokens {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        if(!scan.hasNext()){
+            System.out.println("0");
+            return;
+        }
+        String s = scan.nextLine();
+        scan.close();
+
+        // Write your code here.
+        String[] tokens = s.trim().split("[!,?.*_'@\\ ]+");
+        int size = tokens.length;
+        System.out.println(size);
+        for(int i=0; i<size; i++){
+            System.out.println(tokens[i]);
+        }
+    }
+}
