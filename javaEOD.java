22026a57e6d2d573bca1f440a6e0c5e95005aa8c
Jishnu2608 <jishnudeepborah@gmail.com>
2022-03-06 20:52:59 +0530

    Java End-of-File

---
+++ javaEOD.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class javaEOD {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        String s=sc.nextLine();
+        int i=0;
+        do
+        {
+            System.out.println(i+1 + " " + s);
+            if(sc.hasNextLine())
+            {
+               s=sc.nextLine(); 
+            }
+            else
+            s=null;
+            i=i+1;
+            
+        }while(s!=null);
+    }
+}
