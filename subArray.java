ff4b7c5e5fb64c4f6043e3704e83f6a397b3f550
Jishnu2608 <jishnudeepborah@gmail.com>
2023-05-11 21:31:51 +0530

    Java SubArray

---
+++ subArray.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class subArray {
+
+    public static void main(String[] args) 
+    {
+        Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    int count = 0;
+    int[] a = new int[n];
+    for (int i = 0; i < n; i++){
+        int sum = 0;
+        a[i] = in.nextInt();
+        for (int j = i; j >= 0; j--){
+            sum += a[j];
+            if (sum < 0)
+                count++;
+        }
+    }
+    System.out.println(count);
+    }
+}
