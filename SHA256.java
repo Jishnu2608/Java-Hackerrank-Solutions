8da864220c67e16581d15c21d84f8e427c069e32
Jishnu2608 <jishnudeepborah@gmail.com>
2023-06-20 13:31:34 +0530

    Java SHA-256

---
+++ SHA256.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.security.*;
+
+public class SHA256 {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        Scanner sc = new Scanner(System.in);
+        MessageDigest m = MessageDigest.getInstance("SHA-256");
+        m.reset();
+        m.update(sc.nextLine().getBytes());
+        for (byte i : m.digest()) {
+            System.out.print(String.format("%02x", i));
+        }
+        System.out.println();
+        sc.close(); 
+    }
+}
